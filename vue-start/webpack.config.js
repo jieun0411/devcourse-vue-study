dfc96c42af9ddf9f90465cbb6207b108b4e62701
jieun0411 <annakim0411@naver.com>
2022-05-12 23:53:19 +0900

    Add eslint

---
+++ vue-start/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    extensions: [".vue", ".js"],
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  // vue 파일을 해석하기 위해 loader 작성
+  module: {
+    rules: [
+      {
+        test: /\.vue$/, // 정규표현식
+        use: "vue-loader",
+      },
+      {
+        test: /\.s?css$/, // 정규표현식
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "static" }],
+    }),
+  ],
+};
